53b2b3cdc08499c351750764129b02026cbf5379
khushi1911 <khushikaushik2030@gmail.com>
2026-06-13 20:22:38 +0530

    to print ans

--- sudoku.java
+++ sudoku.java
@@ -1,24 +1,28 @@
 public class sudoku {
 
     public boolean isSafe(char[][] board, int row, int col, int number) {
-        for(int i=0; i<board.length; i++) {
-            if(board[i][col] == (char)(number + '0')) {
+
+        // Check column
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][col] == (char) (number + '0')) {
                 return false;
             }
         }
 
-        for(int j=0; j<board.length; j++) {
-            if(board[row][j] == (char)(number + '0')) {
+        // Check row
+        for (int j = 0; j < board.length; j++) {
+            if (board[row][j] == (char) (number + '0')) {
                 return false;
             }
         }
 
+        // Check 3x3 grid
         int sr = 3 * (row / 3);
         int sc = 3 * (col / 3);
 
-        for(int i=sr; i<sr+3; i++) {
-            for(int j=sc; j<sc+3; j++) {
-                if(board[i][j] == (char)(number + '0')) {
+        for (int i = sr; i < sr + 3; i++) {
+            for (int j = sc; j < sc + 3; j++) {
+                if (board[i][j] == (char) (number + '0')) {
                     return false;
                 }
             }
@@ -28,13 +32,14 @@ public class sudoku {
     }
 
     public boolean helper(char[][] board, int row, int col) {
-        if(row == board.length) {
+
+        if (row == board.length) {
             return true;
         }
 
         int nrow, ncol;
 
-        if(col == board.length - 1) {
+        if (col == board.length - 1) {
             nrow = row + 1;
             ncol = 0;
         } else {
@@ -42,14 +47,16 @@ public class sudoku {
             ncol = col + 1;
         }
 
-        if(board[row][col] != '.') {
+        if (board[row][col] != '.') {
             return helper(board, nrow, ncol);
         } else {
-            for(int i=1; i<=9; i++) {
-                if(isSafe(board, row, col, i)) {
-                    board[row][col] = (char)(i + '0');
 
-                    if(helper(board, nrow, ncol)) {
+            for (int i = 1; i <= 9; i++) {
+                if (isSafe(board, row, col, i)) {
+
+                    board[row][col] = (char) (i + '0');
+
+                    if (helper(board, nrow, ncol)) {
                         return true;
                     }
 
@@ -65,7 +72,37 @@ public class sudoku {
         helper(board, 0, 0);
     }
 
+    public void printBoard(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Sudoku Solver Program");
+
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+
+        sudoku s = new sudoku();
+
+        System.out.println("Original Sudoku:\n");
+        s.printBoard(board);
+
+        s.solveSudoku(board);
+
+        System.out.println("\nSolved Sudoku:\n");
+        s.printBoard(board);
     }
 }
